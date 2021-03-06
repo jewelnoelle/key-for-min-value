45b1071fa88681b220f6334560e11b6c9f5e56e7
Jewel Carrier-Davis <jujicb@gmail.com>
2021-03-06 16:57:26 -0500

    commit tree cc8fc5dfd7c6c6549492f277051417060a30e4f8

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-end
+    values_array = name_hash.collect{|key, value| value}.sort
+    if name_hash == {}
+        return nil
+    else name_hash.each do |key, value|
+        if value == values_array[0]
+            return key
+        else
+            nil
+        end
+    end
+    end
+end 
